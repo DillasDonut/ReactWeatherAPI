8f1f97fc7b7c8d48250331f38c5893a4b5aba898
Arthur Le Berre <arthurleberre@gmail.com>
2021-04-12 14:50:47 +0200

    added .env and api key

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const apiKey = process.env.OPENWEATHER_API_KEY
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`
+
   return (
     <div className="App">
       <header className="App-header">
