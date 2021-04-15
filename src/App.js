d56b3dbd00c93b42b75e49a9d71becf2db3823b9
Arthur Le Berre <arthurleberre@gmail.com>
2021-04-15 15:10:28 +0200

    status display in render ok

--- src/App.js
+++ src/App.js
@@ -6,30 +6,32 @@ function App() {
   // State
   const [apiData, setApiData] = useState({});
   const [getState, setGetState] = useState('paris');
-  const [state, setState] = useState('paris');
+  const [state, setState, result] = useState('paris');
 
   // API KEY AND URL
   const apiKey = process.env.REACT_APP_API_KEY;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
-  const resStatus = apiUrl.status
+  
+  //HTTP STATUS
+  var http = new XMLHttpRequest();
+  http.open('HEAD', apiUrl, false);
+  http.send();
+  console.log('online status : '+http.status)
+  const httpStatus = http.status
 
   // Side effect
   useEffect(() => {
     fetch(apiUrl)
-    .then((res) => {
-      if(!res.ok){
-        console.log('error 404')
-      } 
-      if(res.ok){
-        return res.json()
-        .then((data) => setApiData(data))
+    .then(async (res) => {
+      if(res.ok){     
+        const data = await res.json();
+        return setApiData(data);
       }
     })
-      /* .then((res) => res.json()) */
-      /* .then((data) => setApiData(data)) */
-  }, [apiUrl])
+  }, [apiUrl, setApiData, result])
 
 
+ 
 
   const inputHandler = (event) => {
     setGetState(event.target.value)
@@ -65,16 +67,6 @@ function App() {
     }
   }
 
-
-
-/*   function IncorrectName(props) {  
-    const isNotCorrect = (apiData === 'undefined')
-      return (
-      <h2>Your city name seems to not exist.</h2>
-      )   
-  } */
-
-  
     return (
       <div className="App">
             {apiData.main ? (
@@ -86,6 +78,9 @@ function App() {
               <h2> min : {kelvinToFarenheit(apiData.main.temp_min)}&deg;C / max : {kelvinToFarenheit(apiData.main.temp_max)}&deg;C</h2>
               <h2> wind : {milesToKm(apiData.wind.speed)} Km/h</h2>
               <br/>
+              <div>
+              {httpStatus===404 ? <p>There seems to be a problem with the spelling of the city.</p> : <p>Enter any city name :</p>}
+              </div>
               <input
                 type="text"
                 id="location-name"
